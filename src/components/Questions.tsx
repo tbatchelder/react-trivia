c7abcaac04bf9f3349d94f72465dfe7fe3ea33d2
tbatchelder <pegacorn@optonline.net>
2024-10-07 20:31:26 -0400

    The ugly build

---
+++ src/components/Questions.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Questions = () => {
+  return (
+    <>
+      <div>
+        <div>Question #</div>
+        <div>Answer 1</div>
+        <div>Answer 2</div>
+        <div>Answer 3</div>
+        <div>Answer 4</div>
+        <div>Previous</div>
+        <div>Next</div>
+      </div>
+    </>
+  );
+};
+
+export default Questions;
